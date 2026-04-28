5413cbf9f151bb67793d205476c6b8067140b73a
Ravisanker E <ravisanker1999@gmail.com>
2026-04-28 16:10:11 +0530

    Set default custom function path if not provided (#189)
    Co-authored-by: Ravisanker E <rae@Ravisankers-MacBook-Pro.local>

--- blocks/form/rules/RuleEngineWorker.js
+++ blocks/form/rules/RuleEngineWorker.js
@@ -168,7 +168,7 @@ onmessage = async (e) => {
 
   if (!customFunctionRegistered) {
     const codeBasePath = e?.data?.codeBasePath;
-    const customFunctionPath = e?.data?.payload?.properties?.customFunctionsPath;
+    const customFunctionPath = e?.data?.payload?.properties?.customFunctionsPath || '/blocks/form/functions.js';
     registerCustomFunctions(customFunctionPath, codeBasePath).then(() => {
       customFunctionRegistered = true;
       handleMessageEvent(e);
